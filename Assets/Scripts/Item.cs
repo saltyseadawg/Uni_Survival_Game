be5ebfd9e473db07e803cad0e0ee9377c41feaee
unknown <sabrinayu7@gmail.com>
2019-01-26 17:37:54 -0500

    created Item class

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour {
+    public string name;
+    public int price;
+    public int hp;
+    public int sp;
+    public Item(string newName, int newPrice, int newHP, int newSP)
+    {
+        name = newName;
+        price = newPrice;
+        hp = newHP;
+        sp = newSP;
+    }
+}

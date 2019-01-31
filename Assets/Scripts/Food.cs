91ac8d4fe611ab45aee4dcd99d86343f5bc90f06
Sorano <sabrinayu7@gmail.com>
2019-01-31 00:18:26 -0500

    created Food class, commented out future code that induces bug

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : Item
+{
+    public Food(string newName, int newHP, int newSP, int newPrice) : base(newName, newHP, newSP, newPrice)
+    {
+
+    }
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -14,7 +14,7 @@ public class Inventory : MonoBehaviour
             if (items[i] == null)
             {
                 items[i] = itemToAdd;
-                itemImages[i].sprite = itemToAdd.sprite;
+                //itemImages[i].sprite = itemToAdd.sprite;
                 itemImages[i].enabled = true;
                 return;
             }

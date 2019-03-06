53b2741de3bc6bd0cb9dffd271f0d464e14779e7
Sorano <sabrinayu7@gmail.com>
2019-03-05 23:56:36 -0500

    created CookingManager script

---
+++ Assets/Scripts/CookingManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Manages the display of the cooking menu and cooking activities
+ */ 
+public class CookingManager : MonoBehaviour
+{
+    Inventory inventory;
+    public CookingManager(Inventory player_inventory)
+    {
+        inventory = player_inventory;
+    }
+
+    //Returns a Food item based on Item combinations from Recipes
+    public Food Cook(Item item1, Item item2, Item item3 = null)
+    {
+        //Made static database to store and retrieve recipes from?
+
+        //if combination exists in database return resulting Food
+
+        //else return glop
+        return null;
+    }
+    
+}
